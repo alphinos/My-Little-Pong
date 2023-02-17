5eb337174516c62ac90efdc349a83aa3c19ec578
Aliphinos-Fylakas <pessoal.carlosveras@gmail.com>
2023-02-17 16:03:06 -0300

    Creating the ball, a simple enemy and some collisions

--- pong/source/theater/actor.py
+++ pong/source/theater/actor.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from random import randint
 
 # Theme:
 # Use "Actor" for rects
@@ -8,11 +9,11 @@ class Actor(pygame.sprite.Sprite):
     def __init__(self, width, height):
         super().__init__()
         
-        self.surface = pygame.Surface( (width, height) )
-        self.rect = self.surface.get_rect()
+        self.image = pygame.Surface( (width, height) )
+        self.rect = self.image.get_rect()
         
         self.direction = pygame.math.Vector2(0, 0)
-        self.spd = 8
+        self.spd = 12
     
     def correctDir(self):
         if self.direction.length_squared() == 0 or self.direction.is_normalized():
@@ -23,9 +24,7 @@ class Actor(pygame.sprite.Sprite):
         self.rect.centerx += self.direction.x * self.spd
         self.rect.centery += self.direction.y * self.spd
         
-        
     def update(self):
-        print(self.direction.x, self.direction.y)
         self.correctDir()
         self.move()
         
@@ -44,3 +43,40 @@ class ControlActor(Actor):
             self.direction.y += 1
         if keys[pygame.K_UP] and keys[pygame.K_DOWN]:
             self.direction.y = 0
+
+class AutoActor(Actor):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.target = None
+    
+    def followTarget(self):
+        if self.rect.top > self.target.rect.bottom + self.rect.height // 2:
+            self.direction.y = -1
+        if self.rect.bottom < self.target.rect.top - self.rect.height // 2:
+            self.direction.y = 1
+    
+    def update(self):
+        self.followTarget()
+        self.correctDir()
+        self.move()
+
+class RandomActor(Actor):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.spd = randint(8, 12)
+        self.changeX = self.changeDir(1)
+        self.changeY = self.changeDir(-1)
+        
+        self.direction.x = randint(-100, 101)
+        self.direction.y = randint(-30, 30)
+    
+    def changeDir(self, dir):
+        def Xaxis():
+            self.direction.x *= -1
+        def Yaxis():
+            self.direction.y *= -1
+        
+        if dir == 1:
+            return Xaxis
+        if dir == -1:
+            return Yaxis
--- pong/source/theater/director.py
+++ pong/source/theater/director.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import theater
 import theater.config as config
+from random import randint
 
 class Director:
     def __init__(self):
@@ -11,17 +12,60 @@ class Director:
         self.clock = pygame.time.Clock()
         
         self.player = theater.actor.ControlActor(48, 172)
-        self.player.surface.fill(theater.color.MAGENTA)
+        self.player.image.fill(theater.color.MAGENTA)
         
-        self.enemy = theater.actor.ControlActor(48, 172)
-        self.enemy.surface.fill(theater.color.PURPLE)
+        self.ball = theater.actor.RandomActor(24, 24)
+        self.ball.image.fill(theater.color.PINK)
         
+        self.enemy = theater.actor.AutoActor(48, 172)
+        self.enemy.image.fill(theater.color.PURPLE)
+        self.enemy.target = self.ball
+        
+        self.paddles = pygame.sprite.Group()
+        self.paddles.add(self.player, self.enemy)
+        
+        self.SingleBall = pygame.sprite.GroupSingle()
+        self.SingleBall.add(self.ball)
+        
+        self.ball.rect.center = (config.SCREEN_SIZE[0]/2, config.SCREEN_SIZE[1]/2)
         self.player.rect.center = (config.SCREEN_SIZE[0] - 64, config.SCREEN_SIZE[1]/2 - 24)
         self.enemy.rect.center = (64, config.SCREEN_SIZE[1]/2 - 24)
         
         self.plaudit = theater.plaudit.Plaudit()
         self.plaudit.subscribe(self.player)
-        self.plaudit.subscribe(self.enemy)
+        #self.plaudit.subscribe(self.enemy)
+    
+    def collision(self):
+        for paddle in self.paddles.sprites():
+            if pygame.sprite.spritecollide(paddle, self.SingleBall, False):
+                print("Collided")
+                self.ball.direction.x *= -1
+                # self.ball.direction.y *= -1
+                # if paddle.rect.left < self.ball.rect.right:
+                #     self.ball.direction.x *= -1
+                # if paddle.rect.right > self.ball.rect.left:
+                #     self.ball.direction.x *= -1
+                # if paddle.rect.bottom < self.ball.rect.top:
+                #     self.ball.direction.y *= -1
+                # if paddle.rect.top > self.ball.rect.bottom:
+                #     self.ball.direction.y *= -1
+    
+    def wallCollision(self):
+        if self.ball.rect.x >= config.SCREEN_SIZE[0]:
+            self.reset()
+        if self.ball.rect.x <= 0:
+            self.reset()
+        if self.ball.rect.y >= config.SCREEN_SIZE[1]:
+            self.ball.direction.y *= -1
+        if self.ball.rect.y <= 0:
+            self.ball.direction.y *= -1
+    
+    def reset(self):
+        # Reset the ball to the center of the screen and change its direction
+        self.ball.rect.centerx = config.SCREEN_SIZE[0] // 2 - self.ball.rect.width // 2
+        self.ball.rect.centery = config.SCREEN_SIZE[1] // 2 - self.ball.rect.height // 2
+        self.ball.direction.x = randint(-100, 101)
+        self.ball.direction.y = randint(-30, 30)
     
     def run(self):
         while True:
@@ -32,12 +76,16 @@ class Director:
             
             self.plaudit.update()
             self.player.update()
+            self.ball.update()
             self.enemy.update()
+            
+            self.wallCollision()
+            self.collision()
             
             self.stage.fill("black")
             
-            self.stage.blit(self.player.surface, (self.player.rect.topleft))
-            self.stage.blit(self.enemy.surface, (self.enemy.rect.topleft))
+            self.paddles.draw(self.stage)
+            self.SingleBall.draw(self.stage)
             
             pygame.display.update()
             self.clock.tick(config.FPS)
